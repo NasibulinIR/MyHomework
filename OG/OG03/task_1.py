6d44b43af7260d440e3fd754c505331d81ff087e
NasibulinIR <ilyasnasibulin.work@gmail.com>
2025-07-07 16:42:25 +0700

    added quick_sort function

--- OG/OG03/task_1.py
+++ OG/OG03/task_1.py
@@ -14,5 +14,27 @@ def selection_sort(array):
         sorted_array.append(array.pop(smallest))
     return sorted_array
 
+def quick_sort(array):
+    if len(array) < 2:
+        return array
+
+    first, middle, last = array[0], array[len(array) // 2], array[-1]
+    if (first < middle < last) or (last < middle < first):
+        pivot = middle
+    elif (middle < first < last) or (last < first < middle):
+        pivot = first
+    else:
+        pivot = last
+
+    left = [x for x in array if x < pivot]
+    middle = [x for x in array if x == pivot]
+    right = [x for x in array if x > pivot]
+
+    result = quick_sort(left) + middle + quick_sort(right)
+    return result
+
+
 my_list = [4, 2, 6, 5, 1, 3]
+
+print(quick_sort(my_list))
 print(selection_sort(my_list))
